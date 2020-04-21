ad7591298506fefeabbe9fb8a52565e6bd6917d9
Martin Hurda <33700145+M4R7N@users.noreply.github.com>
2020-04-21 11:56:50 +0200

    Módosítgatások, kiegészítések
    új mappa a játéknak

--- gh70dc.js
+++ gh70dc.js
@@ -26,9 +26,41 @@ function closeMe() {
 	document.getElementById("myModal").style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-/*window.onclick = function(event) {
-  if (event.target == document.getElementById("myModal")) {
-    document.getElementById("myModal").style.display = "none";
+function answerisno() {
+	loadDoc();
+}
+
+
+function loadDoc() {
+	var xhttp = new XMLHttpRequest(); //új xmlhttprequest objektum létrehozása   
+	xhttp.onreadystatechange = function() { 
+		if (this.readyState == 4 && this.status == 200) {
+			help(this);  
+		}   
+	}; 
+ xhttp.open("GET", "games.xml", true); 
+ xhttp.send(); 
+} 
+
+function help(xml) {
+  var xmlDoc = xml.responseXML;
+  var table="<div class='modal-content' id='mc'>";
+  table += "<h2>Hát szóval mivel fiatal vagy, itt van néhány játék:</h2>"
+  table += "<table class='list'><tr><th>Name</th><th>Type</th><th>Link</th></tr>";
+  var x = xmlDoc.getElementsByTagName("GAME");
+  for (var i = 0; i < x.length; i++) { 
+    table += "<tr><td>" +
+	x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+	x[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue +
+    "</td><td><a target='blank' href='" +
+    x[i].getElementsByTagName("LINK")[0].childNodes[0].nodeValue +
+    "'>Link</a></td></tr>";
   }
-} */
+  table += "</table></div>"
+  document.getElementById("myModal").innerHTML = table;
+  var mc = document.getElementById("mc");
+  mc.style.lineHeight = "20px";
+  mc.style.fontFamily = "Corbel";
+  document.getElementById("myModal").innerHTML = table;
+}
